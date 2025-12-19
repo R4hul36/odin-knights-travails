e90af13f38f7bbd6f056bfeb9417b7e58c24c86e
Rahul <rahul.wa47@gmail.com>
2025-12-19 13:44:35 +0530

    added pseudo code for reference

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function knightMoves(start, target) {
+  // pseudo code
+  // create a queue array to store each coordinates
+  let queue = []
+  queue.push(start)
+  //create a an array that stores the predecessors
+  const validMoves = []
+
+  // create a while loop that runs till the quque is empty or if target is reached
+  while(queue.length > 0) {
+    const front = queue.shift()
+    const validMoves = findValidMoves(front)
+
+  }
+
   return start
+}
+
+function findValidMoves (arr) {
+  console.log(arr, "sf")
 }
 
 console.log(knightMoves([0, 0], [1, 2]))
